1ca3ac02feb713d88ac0074f454a94540d6791a9
Ariel <arielabushady@gmail.com>
2021-05-24 17:33:36 -0400

    Add project files.

---
+++ CustomStopwatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Stop_Watch
+{
+    public class CustomStopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _stopTime;
+
+        private bool _isRunning;
+
+        public void Start()
+        {
+            if (_isRunning)
+            {
+                throw new InvalidOperationException($"Stop watch is already running");
+            }
+            else
+            {
+                
+                _startTime = DateTime.Now;
+                _isRunning = true;
+            }
+                                              
+        }
+
+        public void Stop()
+        {
+            if (!_isRunning)
+            {
+                throw new InvalidOperationException($"Stop watch isn't running");
+            }
+            else
+            {
+                _stopTime = DateTime.Now;
+                _isRunning = false;               
+            }
+        }
+
+        public TimeSpan DurationTime()
+        {
+            return _stopTime - _startTime;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stop_Watch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStopwatch watch = new CustomStopwatch();
+            ConsoleKeyInfo info;
+
+            Console.WriteLine($"Press Enter to start the Stop Watch");
+            Console.WriteLine($"Press Backspace to stop the Stop Watch");
+            Console.WriteLine($"Press ESC to exit program");
+
+            try
+            {
+                while (true)
+                {
+                    info = Console.ReadKey(true);
+                    if (info.Key == ConsoleKey.Enter)
+                    {
+                        watch.Start();
+                        Console.WriteLine($"Started...");
+                    }
+                    else if (info.Key == ConsoleKey.Backspace)
+                    {
+                        watch.Stop();
+                        Console.WriteLine($"Stopped...");
+                        Console.WriteLine($"Duration: {watch.DurationTime()}");
+                    }
+                    else if (info.Key == ConsoleKey.Escape)
+                    {
+                        break;
+                    }
+
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
